408b0ea87cf8c8a1ae9bb92eeced1d12d1bbfe64
Patricio <50027994+PatoRocaspana@users.noreply.github.com>
2022-02-07 17:53:17 -0300

    Exercise Two done

--- CSharp-Exercises/ClassesExerciseTwo/Program.cs
+++ CSharp-Exercises/ClassesExerciseTwo/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ClassesExerciseTwo;
+
+var post = new Post
+{
+    CreationDate = DateTime.Today,
+    Title = "This is a title",
+    Description = "This is the content."
+};
+
+Console.WriteLine($"Votes at the beginning: {post.Votes}");
+
+post.DownVoting();
+post.DownVoting();
+Console.WriteLine($"Votes after 2 DownVoting: {post.Votes}");
+
+post.UpVoting();
+post.UpVoting();
+post.UpVoting();
+Console.WriteLine($"Votes after 3 UpVoting: {post.Votes}");
