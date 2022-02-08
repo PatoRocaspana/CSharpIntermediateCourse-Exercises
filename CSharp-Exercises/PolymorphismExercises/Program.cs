f5bb5184c8884f3ca33e46a74235a595b24a4736
Patricio <50027994+PatoRocaspana@users.noreply.github.com>
2022-02-08 09:28:55 -0300

    Firts part of Polymorphism exercise done

--- CSharp-Exercises/PolymorphismExercises/Program.cs
+++ CSharp-Exercises/PolymorphismExercises/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using PolymorphismExercises;
+
+//Exception TimeOut Case
+try
+{
+    var dbConnection = new OracleConnection(connectionString: "Some URL", timeout: TimeSpan.FromMilliseconds(2000));
+    dbConnection.Open();
+    dbConnection.Close();
+}
+catch (TimeoutException e)
+{
+    Console.WriteLine($"{e.Message}");
+}
+
+//Exception ConnectionString Case
+try
+{
+    var dbConnection = new SqlConnection(connectionString:"");
+    dbConnection.Open();
+    dbConnection.Close();
+}
+catch (ArgumentNullException e)
+{
+    Console.WriteLine($"{e.Message}");
+}
+
+//Happy cases!
+//var dbConnection = new OracleConnection(connectionString: "Some URL");
+
+//dbConnection.Open();
+//dbConnection.Close();
---
+++ CSharp-Exercises/PolymorphismExercises/SqlConnection.cs
@@ -0,0 +1,31 @@
+﻿namespace PolymorphismExercises
+{
+    internal class SqlConnection : DbConnection
+    {
+        public SqlConnection(string connectionString) : base(connectionString) { }
+        public SqlConnection(string connectionString, TimeSpan timeout) : base(connectionString, timeout) { }
+
+        public override void Open()
+        {
+            StartTime = DateTime.Now;
+            Thread.Sleep(Timeout);
+
+            if (Timeout.TotalMilliseconds != 0)
+            {
+                if ((DateTime.Now - StartTime) <= Timeout)
+                    Console.WriteLine("SqlConnection was opened");
+                else
+                    throw new TimeoutException($"The timeout was greater than {Timeout} millisenconds");
+            }
+            else
+            {
+                Console.WriteLine("SqlConnection was opened");
+            }
+        }
+
+        public override void Close()
+        {
+            Console.WriteLine("SqlConnection was closed");
+        }
+    }
+}
