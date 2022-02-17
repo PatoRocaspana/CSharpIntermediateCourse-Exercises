25541bd95621b038037c1ea2599bdc5f5d601b7f
Patricio <50027994+PatoRocaspana@users.noreply.github.com>
2022-02-17 11:57:44 -0300

    Interfaces exercises done (#5)
    * Create ConsoleApp project for Interfaces exercises
    
    * Interface Exercises done!

---
+++ CSharp-Exercises/InterfacesExercises/Activities/BuyBusTicket.cs
@@ -0,0 +1,15 @@
+﻿namespace InterfacesExercises.Activities
+{
+    internal class BuyBusTicket : IActivity
+    {        
+        public void Execute()
+        {
+            Console.WriteLine("1- Login shopping portal.");
+            Console.WriteLine("2- Select origin of travel.");
+            Console.WriteLine("3- Select travel destination.");
+            Console.WriteLine("4- Insert travel date.");
+            Console.WriteLine("5- Insert personal and credit card information.");
+            Console.WriteLine("6- logout shopping portal.");
+        }
+    }
+}
---
+++ CSharp-Exercises/InterfacesExercises/Program.cs
@@ -0,0 +1,10 @@
+﻿using InterfacesExercises;
+using InterfacesExercises.Activities;
+
+var cookPizza = new CookPizza();
+var buyBusTicket = new BuyBusTicket();
+
+var workflowEngine = new WorkflowEngine(cookPizza);
+//var workflowEngine = new WorkflowEngine(buyBusTicket);
+
+workflowEngine.Run();
