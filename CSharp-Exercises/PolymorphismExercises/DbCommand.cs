aff20620d4fb040dc6ba47d5b44c60822c26e7a3
Patricio <50027994+PatoRocaspana@users.noreply.github.com>
2022-02-17 11:55:53 -0300

    Polymorphism exercises done (#4)
    * Create ConsoleApp project for Polymorphism exercises
    
    * Firts part of Polymorphism exercise done
    
    * Final part of Polymorphism exercise done

---
+++ CSharp-Exercises/PolymorphismExercises/DbCommand.cs
@@ -0,0 +1,28 @@
+﻿namespace PolymorphismExercises
+{
+    internal class DbCommand
+    {
+        public DbConnection DbConnection { get; private set; }
+        public string Command { get; private set; }
+
+        public DbCommand(DbConnection dbConnection, string command)
+        {
+            if (dbConnection == null)
+                throw new ArgumentNullException(paramName: nameof(dbConnection), message: "DbConnection have a null reference!");
+            else if (command == null || string.IsNullOrWhiteSpace(command))
+                throw new ArgumentNullException(paramName: nameof(command), message: "Command is null or empty!");
+            else
+            {
+                DbConnection = dbConnection;
+                Command = command;
+            }
+        }
+
+        public void Execute()
+        {
+            DbConnection.Open();
+            Console.WriteLine(Command);
+            DbConnection.Close();
+        }
+    }
+}
---
+++ CSharp-Exercises/PolymorphismExercises/Program.cs
@@ -0,0 +1,55 @@
+﻿using PolymorphismExercises;
+
+Console.WriteLine("First part of the exercise");
+
+//Exception TimeOut Case
+try
+{
+    var dbConnectionOne = new OracleConnection(connectionString: "Some URL", timeout: TimeSpan.FromMilliseconds(2000));
+    dbConnectionOne.Open();
+    dbConnectionOne.Close();
+}
+catch (TimeoutException e)
+{
+    Console.WriteLine($"{e.Message}");
+}
+
+//Exception ConnectionString Case
+try
+{
+    var dbConnectionOne = new SqlConnection(connectionString:"");
+    dbConnectionOne.Open();
+    dbConnectionOne.Close();
+}
+catch (ArgumentNullException e)
+{
+    Console.WriteLine($"{e.Message}");
+}
+
+//Happy cases!
+var dbConnectionOk = new OracleConnection(connectionString: "Some URL");
+
+dbConnectionOk.Open();
+dbConnectionOk.Close();
+
+Console.WriteLine("\nSecond Part of the exercise");
+
+//Happy case!
+var dbConnectionSQL = new SqlConnection(connectionString: "some Url");
+var dbConnectionOracle = new OracleConnection(connectionString: "some Url");
+
+var dbCommand = new DbCommand(dbConnection: dbConnectionSQL, command: "SELECT * FROM STACK");
+dbCommand.Execute();
+
+//Exception Case
+try
+{
+    var dbCommandTest = new DbCommand(dbConnection: dbConnectionOracle, command: "");
+    dbCommand.Execute();
+}
+catch (ArgumentNullException e)
+{
+    Console.WriteLine(e.Message);
+}
+
+
