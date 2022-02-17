21d30e313ab6e99df7c4df89890b936ae2d494e4
Patricio <50027994+PatoRocaspana@users.noreply.github.com>
2022-02-17 11:54:44 -0300

    Inheritance Exercise done  (#3)
    * Create ConsoleApp project for Inheritance exercise
    
    * Inheritance Exercise done!

---
+++ CSharp-Exercises/InheritanceExercise/Stack.cs
@@ -0,0 +1,38 @@
+﻿namespace InheritanceExercise
+{
+    internal class Stack
+    {
+        private readonly List<object> _stackList;
+
+        public Stack()
+        {
+            _stackList = new List<object>();
+        }
+        public void Push(object obj)
+        {
+            if (obj != null)
+            {
+                _stackList.Add(obj);
+            }
+            else
+                throw new InvalidOperationException();
+        }
+        public object Pop()
+        {
+
+            if (_stackList.Count != 0)
+            {
+                var lastElement = _stackList.Last();
+                _stackList.Remove(lastElement);
+
+                return lastElement;
+            }
+            else
+                throw new InvalidOperationException();
+        }
+        public void Clear()
+        {
+            _stackList.Clear();
+        }
+    }
+}
